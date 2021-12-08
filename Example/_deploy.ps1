0de25f67cf5c2ce29c88aaa921931d48c369fa67
Granger Godbold <granger.godbold@gmail.com>
2021-12-07 17:30:09 -0700

    INIT - Ignored .binlog and removed "noise" from _deploy.ps1 while making it emit a .binlog.

--- Example/_deploy.ps1
+++ Example/_deploy.ps1
@@ -5,7 +5,6 @@
 	.NOTES
 	I.e.
 		Cleans the final-output folder
-		Cleans the build-intermediates folder.
 		Restores (Nuget packages)
 		Builds in Release mode
 		Finally, deploys/publishes (whatever you're calling it this week)
@@ -23,22 +22,12 @@
 	True/Present - The runtime is packaged into the final executable.
 	False - The .Net runtime must be made available where the executable is ultimately published or it will not be able to run.
 
-	.PARAMETER Clean
-	Specify this switch to do a "dotnet clean" before doing the build.
-	This isn't helpful in automated builds where there isn't any build-intermediates lying around that could affect the build.
-	(I.e. If there are no "bin" or "obj" folders, the "clean" will report a failure, which is just useless noise.)
-
-	.PARAMETER CopyOutputToPath
-	Indicates where to copy the build output/artifacts after a successful build. Make sure this is valid based on your platform (Windows vs *Nix).
-	
 #>
 
 param(
 	[string]$RuntimeId = "win10-x64" 
 	, [string]$FrameworkId = "net6.0"
 	, [switch]$SelfContained 
-	, [switch]$Clean
-	, [string]$CopyOutputToPath = ""
 )
 
 
@@ -63,24 +52,6 @@ if (Test-Path -Path $_OutputDir) {
 Write-Output "";
 
 
-if ($Clean) {
-	Write-Output "Cleaning '$_ProjectFile' build cache ..."; 
-	Start-Process -FilePath "dotnet" -Wait -NoNewWindow -ArgumentList @(
-		, "clean" 
-		, "`"$_ProjectFile`""
-		# don't specify --output; we want it to clean the default "bin" and "obj" directories.
-		, "--configuration Release"
-		, "--framework `"$FrameworkId`"" 
-		, "--runtime `"$RuntimeId`"" # (instead of "-a" or "-os") 
-		, "--nologo" # reduce noise
-	);
-	Write-Output "";
-}
-
-
-# https://docs.microsoft.com/en-us/dotnet/core/deploying/deploy-with-cli
-# https://docs.microsoft.com/en-us/dotnet/core/deploying/single-file
-# https://docs.microsoft.com/en-us/dotnet/core/deploying/trim-self-contained  (We're not going to bother, for now: 2021-10)
 Write-Output "Publishing '$_ProjectFile' in RELEASE mode ..."; 
 Start-Process -FilePath "dotnet" -Wait -NoNewWindow -ArgumentList @(
 	, "publish" # https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
@@ -97,19 +68,9 @@ Start-Process -FilePath "dotnet" -Wait -NoNewWindow -ArgumentList @(
 	, "-p:`"IncludeNativeLibrariesForSelfExtract=true`"" # Put those ~5 "native" dlls into the final .exe also.
 	, "-p:`"DebugType=embedded`"" # put the .pdb files in the .exe also
 	, "-p:`"PublishReadyToRun=true`"" # improve startup performance at the cost of build performance.
-	#, "-p:`"PublishTrimmed=true`"" # This makes the build take a LOT longer, you get a LOT of warnings about possible issues, and it's smaller to just do !SelfContained.
+	, "-bl" # Generate binlogs
 );
 Write-Output "";
-
-
-if (!$CopyOutputToPath) { # catches $null and ""
-	Write-Output "No `$CopyOutputToPath directory specified; skipping copy of build artifacts.";
-} elseif (Test-Path -Path $CopyOutputToPath) {
-	Write-Output "Copying build artifact(s) to '$CopyOutputToPath' ...";
-	Copy-Item -Path ([IO.Path]::Combine($_OutputDir, "*.exe")) -Destination $CopyOutputToPath -Verbose; # Verbose so the output shows exactly what went where.
-} else {
-	Write-Output "Invalid directory specified in `$CopyOutputToPath. Cannot copy build artifact(s) to '$CopyOutputToPath' ";
-}
 
 
 Write-Output "";
